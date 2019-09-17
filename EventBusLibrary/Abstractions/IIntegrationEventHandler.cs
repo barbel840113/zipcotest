ca902c3a49fb6e195983a9781a8661cba98aed6c
Michal Sivak <sivakmichal@gmail.com>
2019-09-17 14:21:31 +1000

    Add project files.

---
+++ EventBusLibrary/Abstractions/IIntegrationEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using EventBusLibrary.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBusLibrary.Interfaces
+{
+    public interface IIntegrationEventHandler<in TIntegrationEvent> : IIntegrationEventHandler 
+
+        where TIntegrationEvent : IntegrationEvent
+    {
+        Task Handle(TIntegrationEvent @event);
+    }
+
+    public interface IIntegrationEventHandler
+    {
+
+    }
+}
---
+++ EventBusLibrary/Events/IntegrationEvent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace EventBusLibrary.Events
+{
+    public class IntegrationEvent
+    {
+        [JsonProperty]
+        public Guid Id { get; private set; }
+
+        [JsonProperty]
+        public DateTime CreationDate { get; set; }
+
+        [JsonConstructor]
+        public IntegrationEvent(Guid id, DateTime createDate) {
+            this.Id = id;
+            this.CreationDate = createDate;
+        }
+
+        public IntegrationEvent()
+        {
+            this.Id = Guid.NewGuid();
+            this.CreationDate = DateTime.Now;                
+        }
+    }
+}
